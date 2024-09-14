ad5eee5da81aa8ed49629de30a4ce73cd584e506
KAIF <mibnzaman@outlook.com>
2024-09-14 19:17:34 +0600

    implemented night mode toggle

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
+// lib/main.dart
 import 'package:flutter/cupertino.dart';
 import 'package:migraine_tracker/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/login.dart'; // Make sure to import your login screen
-import 'screens/home.dart'; // Make sure to import your home screen
+import 'screens/login.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isNightMode = false;
 
   Future<bool> _checkUserLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.containsKey('user');
   }
 
+  void _toggleNightMode() {
+    setState(() {
+      _isNightMode = !_isNightMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      theme: AppTheme.cupertinoTheme,
+      theme: _isNightMode ? AppTheme.darkTheme : AppTheme.lightTheme,
       home: FutureBuilder<bool>(
         future: _checkUserLoggedIn(),
         builder: (context, snapshot) {
