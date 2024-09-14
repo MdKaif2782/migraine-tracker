786c6408d74cd2601ffe3de25682ea3238ec3dc6
KAIF <mibnzaman@outlook.com>
2024-09-14 19:17:06 +0600

    implemented font to theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,41 +2,96 @@
 import 'package:flutter/cupertino.dart';
 
 class AppTheme {
-  static const CupertinoThemeData cupertinoTheme = CupertinoThemeData(
+  static const CupertinoThemeData lightTheme = CupertinoThemeData(
     primaryColor: CupertinoColors.black,
     barBackgroundColor: CupertinoColors.white,
     scaffoldBackgroundColor: CupertinoColors.extraLightBackgroundGray,
+    applyThemeToAll: true,
     textTheme: CupertinoTextThemeData(
       primaryColor: CupertinoColors.black,
       textStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 16.0,
+        fontFamily: 'Poppins',
       ),
       actionTextStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 16.0,
+        fontFamily: 'Poppins',
       ),
       tabLabelTextStyle: TextStyle(
         color: CupertinoColors.inactiveGray,
         fontSize: 10.0,
+        fontFamily: 'Poppins',
       ),
       navTitleTextStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 20.0,
         fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
       ),
       navLargeTitleTextStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 34.0,
         fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
       ),
       pickerTextStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 16.0,
+        fontFamily: 'Poppins',
       ),
       dateTimePickerTextStyle: TextStyle(
         color: CupertinoColors.black,
         fontSize: 16.0,
+        fontFamily: 'Poppins',
+      ),
+    ),
+  );
+
+  static const CupertinoThemeData darkTheme = CupertinoThemeData(
+    primaryColor: CupertinoColors.white,
+    barBackgroundColor: CupertinoColors.black,
+    scaffoldBackgroundColor: CupertinoColors.darkBackgroundGray,
+    applyThemeToAll: true,
+    textTheme: CupertinoTextThemeData(
+      primaryColor: CupertinoColors.white,
+      textStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 16.0,
+        fontFamily: 'Poppins',
+      ),
+      actionTextStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 16.0,
+        fontFamily: 'Poppins',
+      ),
+      tabLabelTextStyle: TextStyle(
+        color: CupertinoColors.inactiveGray,
+        fontSize: 10.0,
+        fontFamily: 'Poppins',
+      ),
+      navTitleTextStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
+      ),
+      navLargeTitleTextStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 34.0,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
+      ),
+      pickerTextStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 16.0,
+        fontFamily: 'Poppins',
+      ),
+      dateTimePickerTextStyle: TextStyle(
+        color: CupertinoColors.white,
+        fontSize: 16.0,
+        fontFamily: 'Poppins',
       ),
     ),
   );
